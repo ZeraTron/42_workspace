2120a45f545f2755148298e114d48237c65a3823
ZeraTron <zeratron@yahoo.com>
2019-08-29 04:54:00 +0200

    blah

--- Toolbox/setup.sh
+++ Toolbox/setup.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
-if grep -q  'export PATH=$PATH":$HOME/Desktop/42_workspace/Toolbox:$HOME/Desktop/42_workspace/Toolbox/template"' ~/.zshrc; then
-	echo 'Toolbox installed !'
+if [ $# -eq 0 ] ; then
+	echo "No target directory supplied"
 	exit
 fi
-echo 'export PATH=$PATH":$HOME/Desktop/42_workspace/Toolbox"' >> ~/.zshrc
-
+_pathname=$1
+if [ ! -d Toolbox ] ; then
+	echo "No existing Toolbox detected"
+	mkdir $1/Toolbox
+	curl -fsSL https://github.com/ZeraTron/42_workspace/tree/master/Toolbox/*
+	echo "Updated Toolbox"
+fi
+if [ $1 = "." ] ; then
+	echo "Install in current working directory..."
+	export _pathname=""
+fi
+if [ $2 = "tmp" ] ; then
+	echo "Temporary Toolbox setup"
+	export PATH="$PATH:$HOME/$_pathname/Toolbox"
+	echo $PATH
+	exit
+fi
+if [ grep -q echo "export PATH=$PATH':$HOME/$_pathname/Toolbox'" ~/.zshrc ] ; then
+	echo 'Toolbox already installed !'
+	exit
+fi
+echo "export PATH=$PATH':$HOME/$1/Toolbox'" >> ~/.zshrc
+echo "[100%] Toolbox installed !"
