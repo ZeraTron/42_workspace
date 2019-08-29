ef6d76ff77d8e89417682057d13484c73b2c21ff
ZeraTron <zeratron@yahoo.com>
2019-08-29 03:46:06 +0200

    Toolbox improvements

--- Toolbox/setup.sh
+++ Toolbox/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if grep -q  'export PATH=$PATH":$HOME/Desktop/42_workspace/Toolbox"' ~/.zshrc; then
-	  echo 'Toolbox installed !'
-	  exit
-  fi
+if grep -q  'export PATH=$PATH":$HOME/Desktop/42_workspace/Toolbox:$HOME/Desktop/42_workspace/Toolbox/template"' ~/.zshrc; then
+	echo 'Toolbox installed !'
+	exit
+fi
 echo 'export PATH=$PATH":$HOME/Desktop/42_workspace/Toolbox"' >> ~/.zshrc
 
